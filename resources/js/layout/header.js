92875c4feba310f19acfc56360d851d48ad32d75
= <=>
2020-05-30 08:20:37 +0100

    Header display protercted routes if logged in

--- resources/js/layout/header.js
+++ resources/js/layout/header.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
 
-export default () => {
-
-
+const Header = ({ state }) => {
     return (
         <header className="header">
             <div className="header-logo"><Link to="/">Gatsby CMS</Link></div>
 
             <nav className="header-navigation">
                 <ul className="header-navigation-ul">
-                    <li className="header-navigation-li"><Link to="/">Home</Link></li>
-                    <li className="header-navigation-li"><Link to="/about/">About</Link></li>
-                    <li className="header-navigation-li"><Link to="/users/">Users</Link></li>
-                    <li className="header-navigation-li"><Link to="/login/">Login</Link></li>
+
+                    { state.token !== null ? (
+                        <>
+                            <li className="header-navigation-li"><Link to="/">Home</Link></li>
+                            <li className="header-navigation-li"><Link to="/about/">About</Link></li>
+                            <li className="header-navigation-li"><Link to="/users/">Users</Link></li>
+                        </>
+                    ) : (
+                        <li className="header-navigation-li"><Link to="/login/">Login</Link></li>
+                    )}
+
                 </ul>
             </nav>
         </header>
     )
 }
+
+const mapStateToProps = ({ auth }) => ({
+    state: {
+        token: auth.token
+    }
+})
+
+export default connect(mapStateToProps)(Header);
