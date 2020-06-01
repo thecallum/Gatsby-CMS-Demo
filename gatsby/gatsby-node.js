b9f04b5cdf16ab02f58ae677403b0689b65a1411
= <=>
2020-06-01 17:45:02 +0100

    Dynamic components loaded into gatsby page template

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,32 +1,36 @@
-const path = require('path')
+const path = require("path")
 
 exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions;    
+  const { createPage } = actions
 
-    const result = await graphql(`
-        query {
-            allPage {
-                edges {
-                    node {
-                        data {
-                        id
-                        content
-                        name
-                        slug
-                        }
-                    }
-                }
+  const result = await graphql(`
+    query {
+      allPage {
+        edges {
+          node {
+            data {
+              id
+              content
+              name
+              slug
+              jsonContent
             }
+          }
         }
-   `);
+      }
+    }
+  `)
 
-    result.data.allPage.edges.forEach((item) => {
-        const page = item.node.data
+  result.data.allPage.edges.forEach(item => {
+    const page = item.node.data
 
-        createPage({
-            path: `/pages/${page.slug}`,
-            component: path.resolve(`./src/templates/page.js`),
-            context: { ...page }
-        })
+    const jsonContent =
+      page.jsonContent === null ? [] : JSON.parse(page.jsonContent)
+
+    createPage({
+      path: `/pages/${page.slug}`,
+      component: path.resolve(`./src/templates/page.js`),
+      context: { ...page, jsonContent },
     })
-};
+  })
+}
--- gatsby/src/templates/page.js
+++ gatsby/src/templates/page.js
@@ -1,25 +1,42 @@
-import React from 'react'
-import Markdown from 'markdown-to-jsx';
-import components from '@components'
-import { Link } from 'gatsby'
+import React from "react"
+import Markdown from "markdown-to-jsx"
+import components from "@components"
+import { Link } from "gatsby"
 
 export default ({ pageContext }) => {
-    return (
-        <div style={{
-            margin: '60px auto',
-            padding: '30px'
-        }}>
-            <p><Link to='/'>Home</Link></p>
+  //   console.log({ pageContext })
 
-            <h1>{ pageContext.name }</h1>
+  return (
+    <div
+      style={{
+        margin: "60px auto",
+        padding: "30px",
+      }}
+    >
+      <p>
+        <Link to="/">Home</Link>
+      </p>
 
-            <Markdown
-                options={{
-                    overrides: { ...components }
-                }}
-            >
-                { pageContext.content }
-            </Markdown>
-        </div>
-    )
+      <h1>{pageContext.name}</h1>
+
+      <Markdown
+        options={{
+          overrides: { ...components },
+        }}
+      >
+        {pageContext.content}
+      </Markdown>
+
+      {/* <pre>{JSON.stringify(pageContext.jsonContent, null, 2)}</pre> */}
+
+      {pageContext.jsonContent.map((component, index) => {
+        const Component = components[component.name]
+        const value = component.hasOwnProperty("value")
+          ? component.value.value
+          : null
+
+        return <Component key={index} props={component.props} value={value} />
+      })}
+    </div>
+  )
 }
