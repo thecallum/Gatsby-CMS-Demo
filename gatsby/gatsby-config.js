23a12faa5b2fb23804392c164a988bfa6441d6f4
= <=>
2020-06-01 19:55:37 +0100

    netlify cache public

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -11,7 +11,12 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-    "gatsby-plugin-netlify-cache",
+    {
+      resolve: "gatsby-plugin-netlify-cache",
+      options: {
+        cachePublic: true,
+      },
+    },
     {
       resolve: `cms`,
       options: {
